d0a8be8ac0fcf32cd214060dcdcc4b2c940bfbb9
Renan Penha Marquette <rpenhamarquette@gmail.com>
2021-04-11 15:12:14 -0300

    Unseful libs removed

--- ConsoleRPG/Program.cs
+++ ConsoleRPG/Program.cs
@@ -1,9 +1,4 @@
-﻿using ConsoleRPG.Mobs;
-using ConsoleRPG.GameComponents;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
+﻿using System;
 
 namespace ConsoleRPG
 {
